00f39526db4099263d0381b738835df93f22f1c3
feedblackg44 <pilmyh@gmail.com>
2021-09-15 01:26:26 +0300

    added js and css files

---
+++ js/script.js
@@ -0,0 +1,12 @@
+function opis() {
+    document.getElementById("frame1").src = "./lab1/opis.html"
+}
+function theme() {
+    document.getElementById("frame1").src = "./lab1/theme.html"
+}
+function structure() {
+    document.getElementById("frame1").src = "./lab1/structure.html"
+}
+function vivod() {
+    document.getElementById("frame1").src = "./lab1/vivod.html"
+}
