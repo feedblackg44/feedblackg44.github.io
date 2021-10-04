8736492a3f383736e1e192af1520e55a76f297e5
feedblackg44 <pilmyh@gmail.com>
2021-10-04 11:25:47 +0300

    lab2 added

---
+++ js/script2.js
@@ -0,0 +1,18 @@
+function opis() {
+    document.getElementById("frame1").src = "./opis.html"
+}
+function lists() {
+    document.getElementById("frame1").src = "./lists.html"
+}
+function ids() {
+    document.getElementById("frame1").src = "./ids.html"
+}
+function margins() {
+    document.getElementById("frame1").src = "./margins.html"
+}
+function swimming() {
+    document.getElementById("frame1").src = "./swimming.html"
+}
+function selectors() {
+    document.getElementById("frame1").src = "./selectors.html"
+}
