1010a8b374274768ae13ec170de505c30c482390
feedblackg44 <pilmyh@gmail.com>
2021-10-04 11:26:05 +0300

    buttons added

--- js/script.js
+++ js/script.js
@@ -1,12 +1,27 @@
-function opis() {
-    document.getElementById("frame1").src = "./lab1/opis.html"
+function lab1() {
+    document.getElementById("lab").src = "./lab1/lab1.html"
 }
-function theme() {
-    document.getElementById("frame1").src = "./lab1/theme.html"
+function lab2() {
+    document.getElementById("lab").src = "./lab2/lab2.html"
 }
-function structure() {
-    document.getElementById("frame1").src = "./lab1/structure.html"
+/*function lab3() {
+    document.getElementById("lab").src = "./lab3/lab3.html"
 }
-function vivod() {
-    document.getElementById("frame1").src = "./lab1/vivod.html"
+function lab4() {
+    document.getElementById("lab").src = "./lab4/lab4.html"
 }
+function lab5() {
+    document.getElementById("lab").src = "./lab5/lab5.html"
+}
+function lab6() {
+    document.getElementById("lab").src = "./lab6/lab6.html"
+}
+function lab7() {
+    document.getElementById("lab").src = "./lab7/lab7.html"
+}
+function lab8() {
+    document.getElementById("lab").src = "./lab8/lab8.html"
+}
+function lab9() {
+    document.getElementById("lab").src = "./lab9/lab9.html"
+}*/
